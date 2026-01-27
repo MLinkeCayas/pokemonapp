dfa37f29d087ddc085dcbc89955f6eeba27d744c
Christoph Stock <christoph.stock@ottobock.com>
2026-01-27 16:05:36 +0100

    Refactor LoadImage function for improved readability and consistency in formatting

--- components/pokemon-image.tsx
+++ components/pokemon-image.tsx
@@ -1,17 +1,25 @@
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet } from "react-native";
 
-export async function LoadImage(imagePath: string, imageHeight: number, imageWidth: number) {
-
-    return <Image
-        source={{uri: imagePath}}
-        style={[ styles.imageHeader, { width: imageWidth, height: imageHeight, flex: 1 }]}
+export async function LoadImage(
+  imagePath: string,
+  imageHeight: number,
+  imageWidth: number,
+) {
+  return (
+    <Image
+      source={{ uri: imagePath }}
+      style={[
+        styles.imageHeader,
+        { width: imageWidth, height: imageHeight, flex: 1 },
+      ]}
     />
+  );
 }
 
 const styles = StyleSheet.create({
   imageHeader: {
     flex: 1,
     width: 100,
-    height: 100
+    height: 100,
   },
 });
