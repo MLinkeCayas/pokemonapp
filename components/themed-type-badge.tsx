6b96700d1c9ca5a8db6b35752f32b6098060eb61
Christoph Stock <christoph.stock@ottobock.com>
2026-01-28 11:51:22 +0100

    Refactor ThemedTypeBadge component for improved readability
    Adjusted formatting in the ThemedTypeBadge component by adding line breaks for better code clarity. Standardized string quotes in the styles definition for consistency.

--- components/themed-type-badge.tsx
+++ components/themed-type-badge.tsx
@@ -63,7 +63,9 @@ export function ThemedTypeBadge({
       lightColor={typeColor}
       darkColor={typeColor}
     >
-      <ThemedText style={styles.badgeText}>{pokemonType.toUpperCase()}</ThemedText>
+      <ThemedText style={styles.badgeText}>
+        {pokemonType.toUpperCase()}
+      </ThemedText>
     </ThemedView>
   );
 }
@@ -76,13 +78,13 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
   },
-  badge: { width: 80, borderRadius: 40, borderColor: 'black', borderWidth: 2 },
+  badge: { width: 80, borderRadius: 40, borderColor: "black", borderWidth: 2 },
   badgeText: {
     alignSelf: "center",
     fontSize: 14,
-    color: 'white',
-    textShadowColor: 'black',
+    color: "white",
+    textShadowColor: "black",
     textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 2
+    textShadowRadius: 2,
   },
 });
