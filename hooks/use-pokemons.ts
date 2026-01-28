f07d7ca41bfc7c70bc24d8390c9437621094f4d3
Martin Luong <m.luong@cayas.de>
2026-01-28 10:18:08 +0100

    First implementation details page

--- hooks/use-pokemons.ts
+++ hooks/use-pokemons.ts
@@ -1,9 +1,16 @@
-import { getPokemons } from "@/services/pokeapi";
+import { getPokemonById, getPokemons } from "@/services/pokeapi";
 import { useQuery } from "@tanstack/react-query";
 
 export function usePokemons(pageSize: number, page: number) {
   return useQuery({
     queryKey: ["pokemons", pageSize, page],
     queryFn: () => getPokemons(pageSize, page),
+  });
+}
+
+export function usePokemon(pokemonId: number) {
+  return useQuery({
+    queryKey: ["pokemon", pokemonId],
+    queryFn: () => getPokemonById(pokemonId),
   });
 }
