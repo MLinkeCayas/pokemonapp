4498942484bff7bf7a633d1a1964ec1474e32793
Martin Luong <m.luong@cayas.de>
2026-01-28 17:25:00 +0100

    small animation

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -4,21 +4,34 @@ import { ThemedTypeBadge } from "@/components/themed-type-badge";
 import { ThemedView } from "@/components/themed-view";
 import { usePokemon } from "@/hooks/use-pokemons";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import { useEffect } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { Animated, ScrollView, StyleSheet, View } from "react-native";
 
 export default function PokemonDetailScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const navigation = useNavigation();
   const { data, isLoading, error } = usePokemon(parseInt(id, 10));
 
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   useEffect(() => {
     // Set the title dynamically based on the Pokemon ID
     if (data?.name) {
       navigation.setOptions({
-        title: `Pokemon ${data?.name as string}`,
+        title: `Pokemon ${data?.name.toUpperCase() as string}`,
       });
+
+      fadeIn();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.name, navigation]);
 
   if (isLoading) return <ThemedText type="title">Loading...</ThemedText>;
@@ -34,12 +47,21 @@ export default function PokemonDetailScreen() {
         showsVerticalScrollIndicator={true}
       >
         <ThemedView style={styles.centeredSection}>
-          <PokemonImage
-            imagePath={data?.sprites.front_default as string}
-            imageHeight={200}
-            imageWidth={200}
-            style={styles.pokemonImage}
-          />
+          <Animated.View
+            style={[
+              styles.container,
+              {
+                opacity: fadeAnim,
+              },
+            ]}
+          >
+            <PokemonImage
+              imagePath={data?.sprites.front_default as string}
+              imageHeight={200}
+              imageWidth={200}
+              style={styles.pokemonImage}
+            />
+          </Animated.View>
 
           <ThemedText type="title" style={styles.pokemonName}>
             # {data?.id} {data?.name?.toUpperCase()}
